60043fc2a9d42a749172e97ebbca2f937360d5f1
AlvisOu <oualvis@gmail.com>
2025-02-16 18:09:22 -0500

    Repurposing the database for historical data, not simulation

--- fantasy-football-app/simulation_service/database/seed.py
+++ fantasy-football-app/simulation_service/database/seed.py
@@ -1,65 +1,79 @@
 import requests, argparse
 from simulation_service.database.models import SessionLocal, PlayerData
 from simulation_service.database.db_setup import init_db
-from simulation_service.app.utils.lineup_fetcher import fetch_fantasy_lineup
+# from simulation_service.app.utils.lineup_fetcher import fetch_fantasy_lineup
 
 def fetch_external_data(player_id):
     """Fetch player data from an external API given a player ID."""
     current_year = 2024
-    player = {}
-    player['id'] = player_id
+    tuesdays = {} # date to week number
+
+    data = []
 
     player_url = f"https://watsonfantasyfootball.espn.com/espnpartner/dallas/players/players_{player_id}_ESPNFantasyFootball_{current_year}.json"
     player_response = requests.get(player_url)
-    player_data = player_response.json()[-1]
-    player['name'] = player_data['FULL_NAME']
-    player['position'] = player_data['POSITION']
-    player['team'] = player_data['TEAM']
-    player['projected_score'] = player_data['OUTSIDE_PROJECTION']
+    player_data = player_response.json()
 
     classifiers_url = f"https://watsonfantasyfootball.espn.com/espnpartner/dallas/classifiers/classifiers_{player_id}_ESPNFantasyFootball_{current_year}.json"
     classifier_response = requests.get(classifiers_url)
-    classifier_data = classifier_response.json()[-2:]
-    player['bust_probability'] = classifier_data[0]['NORMALIZED_RESULT']
-    player['boom_probability'] = classifier_data[1]['NORMALIZED_RESULT']
+    classifier_data = classifier_response.json()
 
-    # projections_url = f"https://watsonfantasyfootball.espn.com/espnpartner/dallas/projections/projections_{player_id}_ESPNFantasyFootball_{current_year}.json"
-    # projections_response = requests.get(projections_url)
+    for entry in player_data:
+        player = {}
+        player['player_id'] = player_id
+        player['name'] = entry['FULL_NAME']
+        player['position'] = entry['POSITION']
+        player['team'] = entry['TEAM']
+        player['projected_score'] = entry['OUTSIDE_PROJECTION']
+        data.append(player)
 
-    return player
+    for entry in classifier_data:
+        if entry['DATA_TIMESTAMP'] in tuesdays:
+            player = data[tuesdays[entry['DATA_TIMESTAMP']]]
+            if entry['MODEL_TYPE'] == 'bust_classifier':
+                player['bust_probability'] = entry['NORMALIZED_RESULT']
+            elif entry['MODEL_TYPE'] == 'boom_classifier':
+                player['boom_probability'] = entry['NORMALIZED_RESULT']
+
+    # Need an api to get actual points, injury status, and, if possible, what fantasy team they were on and lineup status
+            
+    return data
 
 def seed_database(player_ids):
-    """Seed the database with player data."""
+    """
+    Seed the database with player data.
+    """
     init_db()
     session = SessionLocal()
 
     for id in player_ids:
         player = fetch_external_data(id)
 
-        player_record = PlayerData(
-            id=player['id'],
-            player_name=player['name'],
-            position=player['position'],
-            team=player['team'],
-            projected_score=player['projected_score'],
-            boom_probability=player['boom_probability'] if player['boom_probability'] is not None else 0.0,
-            bust_probability=player['bust_probability'] if player['bust_probability'] is not None else 0.0,
-            lineup_status=player_ids[id][0],
-            injury_status=player_ids[id][1],
-        )
-
-        session.merge(player_record)
+        for i in range(len(player)):
+            
+            player_record = PlayerData(
+                player_id=player['player_id'],
+                week = i + 1,
+                player_name=player['name'],
+                position=player['position'],
+                team=player['team'],
+                projected_score=player['projected_score'],
+                boom_probability=player['boom_probability'] if player['boom_probability'] is not None else 0.0,
+                bust_probability=player['bust_probability'] if player['bust_probability'] is not None else 0.0,
+                actual_score=player['actual_score'] if player['actual_score'] is not None else 0.0,
+                injury_status=player['injury_status'] if player['injury_status'] is not None else "N/A",
+                lineup_status=player['lineup_status'] if player['lineup_status'] is not None else "N/A",
+                fantasy_team=player['fantasy_team'] if player['fantasy_team'] is not None else "N/A",
+            )
+            session.add(player_record)
 
     session.commit()
     session.close()
     print("Database seeded with player data!")
 
 def main():
-    parser = argparse.ArgumentParser(description='Fetch a fantasy lineup.')
-    parser.add_argument('team_id', type=int, help='The team ID of the fantasy lineup to fetch.')
-    args = parser.parse_args()
-    res = fetch_fantasy_lineup(args.team_id)
-    seed_database(res)
+    ids = []
+    seed_database(ids)
 
 
 if __name__ == "__main__":
