a4bd6be41b0f73a2425f9b7d995fbb3581ee77cd
AlvisOu <oualvis@gmail.com>
2025-02-19 18:28:05 -0500

    Refactored the dependency code to have better cleanup

--- fantasy-football-app/simulation_service/app/dependencies.py
+++ fantasy-football-app/simulation_service/app/dependencies.py
@@ -5,4 +5,7 @@ REDIS_URL = "redis://localhost:6379"
 async def get_redis():
     """ Dependency injection to get a redis connection connection """
     redis = await aioredis.create_redis_pool(REDIS_URL, decode_responses=True)
-    return redis
+    try:
+        yield redis
+    finally:
+        await redis.close()
