982bedfd2d7a277f89c17f966a3dfdca34be6ca7
AlvisOu <oualvis@gmail.com>
2025-02-19 18:29:12 -0500

    Better code formatting for future deployment

--- fantasy-football-app/simulation_service/app/dependencies.py
+++ fantasy-football-app/simulation_service/app/dependencies.py
@@ -1,10 +1,12 @@
 import aioredis
 
-REDIS_URL = "redis://localhost:6379"
+# Redis connection settings
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
 
 async def get_redis():
     """ Dependency injection to get a redis connection connection """
-    redis = await aioredis.create_redis_pool(REDIS_URL, decode_responses=True)
+    redis = await aioredis.create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}", decode_responses=True)
     try:
         yield redis
     finally:
