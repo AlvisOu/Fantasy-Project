1a1dd3a318a3ab4712ebeec8d07dcceb9b79829c
AlvisOu <oualvis@gmail.com>
2025-02-19 18:25:04 -0500

    Set up main.py and dependencies.py (for redis dependency injection)

---
+++ fantasy-football-app/simulation_service/app/dependencies.py
@@ -0,0 +1,8 @@
+import aioredis
+
+REDIS_URL = "redis://localhost:6379"
+
+async def get_redis():
+    """ Dependency injection to get a redis connection connection """
+    redis = await aioredis.create_redis_pool(REDIS_URL, decode_responses=True)
+    return redis
