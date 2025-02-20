f7337cd3d999ab3c7c9989d4fab2852c8ac4a77e
AlvisOu <oualvis@gmail.com>
2025-02-20 16:01:47 -0500

    Added comments

--- fantasy-football-app/simulation_service/app/dependencies.py
+++ fantasy-football-app/simulation_service/app/dependencies.py
@@ -6,7 +6,7 @@ REDIS_PORT = 6379
 
 async def get_redis():
     """ Dependency injection to get a redis connection connection """
-    redis = await aioredis.create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}", decode_responses=True)
+    redis = await aioredis.create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}", decode_responses=True) # decode_responses=True to convert bytes to strings
     try:
         yield redis
     finally:
